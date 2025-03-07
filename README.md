dc3d21a25feaa85325bbd037bdbf1e1a07f4a65a
maxgallup <maxgallup@pm.me>
2025-03-07 20:22:31 +0100

    feat: simplified example

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # service-template-python
-Create a rover service in Python quickly with this template
+This template serves as a starting point for writing a service in python. It uses [roverlib](https://pypi.org/project/roverlib/) to make setting up communication with other services easier.
+
+This example service acts like a "controller" so it takes as input data from [imaging](https://github.com/VU-ASE/imaging) and outputs data for the [actuator](https://github.com/VU-ASE/actuator). For simplicity, it does nothing with the image data, but simply starts driving in a snake pattern. For details, take a look at [`src/main.py`](./src/main.py).
+
+## Uploading
+
+After changing the name, author and version in the [service.yaml](./service.yaml) you can use the following command to upload the service to the rover (in this case to Rover 5):
+
+``` bash
+roverctl -r 5 upload .
+```
+
+For more fast-paced development you can add `-w` which will watch for changes and automatically upload them.
+
+``` bash
+roverctl -r 5 upload . -w
+```
+
+## Running a Pipeline
+To run this in a pipeline, open the web interface with `roverctl -r 5` and make sure you enable the dependencies that this service requires, namely `vu-ase/imaging` and `vu-ase/actuator`.
+
--- src/main.py
+++ src/main.py
@@ -1,96 +1,97 @@
 #!/usr/bin/python3
-import roverlib
+
+import time
 import signal
 from loguru import logger
+import roverlib
 import roverlib.rovercom as rovercom
-import time
 
-
+# 
 def run(service : roverlib.Service, configuration : roverlib.ServiceConfiguration):
     if configuration is None:
         return ValueError("Configuration cannot be accessed")
     
-    #
-    # Access the service indendity, who am I?
-    #
+    # Access the to the identity of this service, who am I?
     logger.info(f"Hello World, a new service {service.name} was born at version {service.version}")
 
-    #
-    # Access the service configuration, to use runtime parameters
-    #
+    # Access the service configuration found in service.yaml, to use runtime parameters
     example_num = configuration.GetFloatSafe("number-example")
-    logger.info(f"Fetched runtime configuration example number: {example_num}")
-
     example_string = configuration.GetStringSafe("string-example")
-    logger.info(f"Fetched runtime configuration example string: {example_string}")
-
     example_string_tunable = configuration.GetStringSafe("tunable-string-example")
-    logger.info(f"Fetched runtime configuration example tunable sring: {example_string_tunable}")
+    logger.info(f"Fetched runtime configuration examples: ")
+    logger.info(f"number: {example_num}")
+    logger.info(f"string: {example_string}")
+    logger.info(f"tunable string: {example_string_tunable}")
 
-    #
-    # Writing to an output that other services can read (see service.yaml to understand the output name)
-    #
-    write_stream : roverlib.WriteStream = service.GetWriteStream("example-output")
 
-    # Try to write a simple rovercom message, as if we are sending RPM data
-    write_stream.Write(rovercom.SensorOutput(
-        sensor_id=404,
-        timestamp=int(time.time() * 1000),
-        status=0,
-        rpm_ouput=rovercom.RpmSensorOutput(
-            left_rpm=1000,
-            right_rpm=1200,
-        )
-    ))
 
-    # You don't like using protobuf messages? No problem, you can write raw bytes too
-    write_stream.WriteBytes(b"Hello world!")
+    # This service will be reading the "path" data from the imaging service
+    # and it will publish "decision" data to anyone who subscribes.
+    # We initialize the streams as follows:
+    image_stream : roverlib.ReadStream = service.GetReadStream("imaging", "path")
+    actuator_stream : roverlib.WriteStream = service.GetWriteStream("decision")
 
-    # 
-    # Reading from an input, to get data from other services (see service.yaml to understand the input name)
-    #
-    read_stream : roverlib.ReadStream = service.GetReadStream("example-input", "rpm-data")
-
-    # Try to read a simple rovercom message, as if we are receiving RPM data
-    pb_data = read_stream.Read()
-
-    # Find out if we actually have rpm data
-    if pb_data.rpm_ouput is None:
-        logger.error("expected RPM data, but got something else")
-    else:
-        logger.info(f"Received RPM data: {pb_data.rpm_ouput.left_rpm}, {pb_data.rpm_ouput.right_rpm}")
-
-    # You don't like using protobuf messages? No problem, you can read raw bytes too
-    raw_data = read_stream.ReadBytes()
-    logger.info(f"Received raw data: {str(raw_data)}")
-
-    #
-    # Now do something else fun, see if our "example-string-tunable" is updated
-    #
-    curr = example_string_tunable
+    # Main processing loop
     while True:
-        logger.info("Checking for tunable string update")
+        # --------------------------------
+        # Reading data from other services
+        # --------------------------------
+        # Then read a message from imaging. Typically, the next line will be at the start of your loop
+        image_data = image_stream.Read()
+        if image_data.camera_output is None:
+            logger.error("image data was not camera output")
+            exit(1)
+        
+        # For more information on what available messages are published, check the rovercom
+        # definition https://github.com/VU-ASE/roverlib-python/blob/main/src/roverlib/rovercom.py
+        width = image_data.camera_output.trajectory.width
+        height = image_data.camera_output.trajectory.height
+        logger.info(f"Received an image of {width} x {height} pixels")
 
-        # We are not using the safe version here, because using locks is boring
-		# (this is perfectly fine if you are constantly polling the value)
-		# nb: this is not a blocking call, it will return the last known value
-        new_val = configuration.GetString("tunable-string-example")
+        for point in image_data.camera_output.trajectory.points:
+            logger.info(f"imaging produced a point: {point}")
 
-        if curr is not new_val:
-            logger.info(f"Tunable string updated: {curr} -> {new_val}")
+        # -------------------------
+        # Writing to other services
+        # -------------------------
 
-        # Don't waste CPU cycles
         time.sleep(1)
 
+        actuator_stream.Write(rovercom.SensorOutput(
+            sensor_id=2,
+            timestamp=int(time.time() * 1000),
+            controller_output=rovercom.ControllerOutput(
+                steering_angle = -0.4,
+                left_throttle = 0.12,
+                right_throttle = 0.12,
+            )
+        ))
+
+        time.sleep(1)
+
+        actuator_stream.Write(rovercom.SensorOutput(
+            sensor_id=2,
+            timestamp=int(time.time() * 1000),
+            controller_output=rovercom.ControllerOutput(
+                steering_angle = 0.4,
+                left_throttle = 0.12,
+                right_throttle = 0.12,
+            )
+        ))
+
+
+# This function gets called when the pipeline gets terminated. This can happen if any other service
+# in the pipeline crashes or if the pipeline is stopped via the the web interface.
 def on_terminate(sig : signal):
     logger.info(f"signal: {str(sig)}, Terminating service")
 
     #
     # ...
-    # Any clean up logic here
+    # If you need to add clean up logic, add it here
     # ...
     #
 
     return None
 
-roverlib.Run(run, on_terminate)
+if __name__ == "__main__":
+    roverlib.Run(run, on_terminate)
